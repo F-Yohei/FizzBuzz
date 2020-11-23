8cf57895a7c98550ded2c8c919626d753cbac310
Yohei Fukudome <pt83s0502@gmail.com>
2020-11-23 23:31:06 +0900

    if文とfor文を使ってFizzBuzzの作成

--- main.js
+++ main.js
@@ -1,12 +1,39 @@
 const button = document.getElementById("output");
 
 button.addEventListener("click", () => {
-  let fizzNum = document.getElementById("fizzNum").value * 2;
-  let buzzNum = document.getElementById("buzzNum").value * 2;
+  const fizzNum = document.getElementById("fizzNum").value;
+  const buzzNum = document.getElementById("buzzNum").value;
+  const fizzbuzzList = document.getElementById("fizzbuzzList");
 
-  if (fizzNum % 2 === 0) {
-    console.log("fizz");
-  } else if (buzzNum % 2 === 0) {
-    console.log("buzz");
+  for (i = fizzNum, j = buzzNum; i <= 100; i *= 2, j *= 2) {
+    const fizzli = document.createElement("li");
+    fizzli.innerHTML = "Fizz" + i;
+    fizzbuzzList.appendChild(fizzli);
+
+    const buzzli = document.createElement("li");
+    buzzli.innerHTML = "Buzz" + j;
+    fizzbuzzList.appendChild(buzzli);
   }
 });
+
+if (fizzNum === "" || typeof fizzNum === "string") {
+  const test = document.createElement("li");
+  test.innerHTML = "整数値を入力してください";
+  fizzbuzzList.appendChild(test);
+} else if (buzzNum === "" || typeof buzzNum === "string") {
+  const test = document.createElement("li");
+  test.innerHTML = "整数値を入力してください";
+  fizzbuzzList.appendChild(test);
+}
+
+//もしfizzNumとbuzzNumの値が同じだったらFizzBuzz（数字）と表示させる。
+// if (i === j) {
+//     fizzli.innerHTML = "FizzBuzz" + i;
+//     fizzbuzzList.appendChild(fizzli);
+//   }
+
+// if (i % fizzNum === 0 && j % buzzNum === 0) {
+//     const fizzli = document.createElement("li");
+//     fizzbuzzList.appendChild(fizzli);
+//     fizzli.innerHTML = "FizzBuzz" + i;
+//   }
