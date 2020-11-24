a88c62a4ad09ad75383653b1379c28bff2c18227
Yohei Fukudome <pt83s0502@gmail.com>
2020-11-24 23:46:47 +0900

    if文でエラーメッセージを表示する処理を追加

--- main.js
+++ main.js
@@ -1,32 +1,59 @@
 const button = document.getElementById("output");
+const fizzbuzzList = document.getElementById("fizzbuzzList");
+
+const listAdd = (a) => {
+  const li = document.createElement("li");
+  li.innerHTML = a;
+  fizzbuzzList.appendChild(li);
+  console.log(li);
+};
 
 button.addEventListener("click", () => {
   const fizzNum = document.getElementById("fizzNum").value;
   const buzzNum = document.getElementById("buzzNum").value;
-  const fizzbuzzList = document.getElementById("fizzbuzzList");
 
-  for (i = fizzNum, j = buzzNum; i <= 100; i *= 2, j *= 2) {
-    const fizzli = document.createElement("li");
-    fizzli.innerHTML = "Fizz" + i;
-    fizzbuzzList.appendChild(fizzli);
+  if (fizzNum === "" || typeof fizzNum === "string") {
+    listAdd("整数値を入力してください");
+  } else if (buzzNum === "" || typeof buzzNum === "string") {
+    listAdd("整数値を入力してください");
+  }
 
-    const buzzli = document.createElement("li");
-    buzzli.innerHTML = "Buzz" + j;
-    fizzbuzzList.appendChild(buzzli);
+  for (i = 1; i < 100; i++) {
+    if (i % fizzNum === 0 && i % buzzNum === 0) {
+      listAdd("FizzBuzz" + i);
+    } else if (i % fizzNum === 0) {
+      listAdd("Fizz" + i);
+    } else if (i % buzzNum === 0) {
+      listAdd("Buzz" + i);
+    }
   }
 });
 
-if (fizzNum === "" || typeof fizzNum === "string") {
-  const test = document.createElement("li");
-  test.innerHTML = "整数値を入力してください";
-  fizzbuzzList.appendChild(test);
-} else if (buzzNum === "" || typeof buzzNum === "string") {
-  const test = document.createElement("li");
-  test.innerHTML = "整数値を入力してください";
-  fizzbuzzList.appendChild(test);
-}
+// button.addEventListener("click", () => {
+//   const fizzbuzzList = document.getElementById("fizzbuzzList");
 
-//もしfizzNumとbuzzNumの値が同じだったらFizzBuzz（数字）と表示させる。
+//   for (i = fizzNum, j = buzzNum; i <= 100; i *= 2, j *= 2) {
+//     const fizzli = document.createElement("li");
+//     fizzli.innerHTML = "Fizz" + i;
+//     fizzbuzzList.appendChild(fizzli);
+
+//     const buzzli = document.createElement("li");
+//     buzzli.innerHTML = "Buzz" + j;
+//     fizzbuzzList.appendChild(buzzli);
+//   }
+// });
+
+// if (fizzNum === "" || typeof fizzNum === "string") {
+//   const test = document.createElement("li");
+//   test.innerHTML = "整数値を入力してください";
+//   fizzbuzzList.appendChild(test);
+// } else if (buzzNum === "" || typeof buzzNum === "string") {
+//   const test = document.createElement("li");
+//   test.innerHTML = "整数値を入力してください";
+//   fizzbuzzList.appendChild(test);
+// }
+
+// もしfizzNumとbuzzNumの値が同じだったらFizzBuzz（数字）と表示させる。
 // if (i === j) {
 //     fizzli.innerHTML = "FizzBuzz" + i;
 //     fizzbuzzList.appendChild(fizzli);
